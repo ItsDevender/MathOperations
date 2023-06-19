998997f2a7b1cf4f268f7e41824e869e8d420830
Devender Singh <32868104+ItsDevender@users.noreply.github.com>
2023-06-19 21:18:45 +0530

    Update MessgaeBuilderTest.java
    removing public modifier as non-compliant issue raised by sonarqube

--- src/test/java/math/operation/MessgaeBuilderTest.java
+++ src/test/java/math/operation/MessgaeBuilderTest.java
@@ -4,10 +4,10 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class MessgaeBuilderTest {
+class MessgaeBuilderTest {
 
     @Test
-    public void testNameMkyong() {
+    void testNameMkyong() {
 
         MessageBuilder obj = new MessageBuilder();
         assertEquals("Hello Devender", obj.getMessage("Devender"));
