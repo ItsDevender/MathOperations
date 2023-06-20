10edb92162e28af5ba0704e31ab52443fde284d2
Devender Singh <32868104+ItsDevender@users.noreply.github.com>
2023-06-20 16:46:49 +0530

    Update ArithmeticOperationsTest.java

--- src/test/java/math/operation/ArithmeticOperationsTest.java
+++ src/test/java/math/operation/ArithmeticOperationsTest.java
@@ -3,10 +3,10 @@ package math.operation;
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
-class ArithmeticOperationsTest {
+public class ArithmeticOperationsTest {
 
 	@Test
-	void testAdd()
+	public void testAdd()
 	{	
 		ArithmeticOperations operations = new ArithmeticOperations();
 		Integer actual = operations.add(2, 6);
